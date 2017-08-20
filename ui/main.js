0640f91caefb57faef404bf99c887bdcb2c3994b
phani0811 <30619088+phani0811@users.noreply.github.com>
2017-08-20 11:36:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,9 +24,10 @@ button.onclick = function () {
 //Submit Name
 
 
-var submit = document.getElementById('submit-btn');
+var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
      if (request.readyState === XMLHttpRequest.DONE)
     {
         if(request.status === 200)
@@ -43,7 +44,6 @@ submit.onclick = function () {
     }
 }
 };
-
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 
